8ed9e3290ccc2d26ec39e848cad9c67def505f4e
simontegg <simon@enspiral.com>
2017-03-13 09:33:01 +1300

    hello

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('boo')
+/**
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloHttp = function helloHttp (req, res) {
+  res.send(`Hello ${req.body.name || 'World'}!`);
+};
