4fbd80f23ed83e830dd64db6f41bb5613a0a59ac
simontegg <simon@enspiral.com>
2017-03-13 11:27:44 +1300

    tweak-res

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const config = require('./config.json')
+
+
+
 /**
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
 exports.helloHttp = function helloHttp (req, res) {
-  res.send(`959413515`);
-};
+   if (req.query['hub.verify_token'] === config.verifyToken) {
+      res.send(req.query['hub.challenge'])
+   } else {
+      res.send('Error, wrong validation token');    
+   }
+}
